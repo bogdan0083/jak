15ffdde30c2f52ba8644918ccd43e4544e981c0a
Bogdan <d.bogdan@inbox.ru>
2016-11-24 17:45:00 +0700

    progress first page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,6 +38,49 @@ $(document).ready(function () {
         $(this).toggleClass('active');
         $('.catalog-nav').toggleClass('js-active');
     });
+
+    if ($(window).width() > 880) {
+
+        $('.mega-slider').slick({
+            slidesToShow: 1,
+            infinite: true,
+            pauseOnHover: false,
+            dots: true
+        });
+        
+        $('.mega-slider').on('init', function () {
+            
+        });
+        //
+        // $('.about-slider .wrapper').slick({
+        //     slidesToShow: 1,
+        //     infinite: true,
+        //     autoplay: true,
+        //     pauseOnHover: false,
+        //     prevArrow: $('.about-slider .long-arrow-left'),
+        //     nextArrow: $('.about-slider .long-arrow-right')
+        // });
+        //
+        // $('.about-slider .wrapper').on('beforeChange', function(e, slick, cur, next) {
+        //     $('.js-slider-count').find('a').removeClass('active').eq(next).addClass('active');
+        // });
+        //
+        // $('.js-slider-count').on('click', 'a', function(e){
+        //     e.preventDefault();
+        //
+        //     $('.about-slider .wrapper').slick('slickGoTo', $(this).index());
+        // });
+        //
+        //
+        // $('.new-items-slider').slick({
+        //     slidesToShow: 1,
+        //     dots: true,
+        //     infinite: true,
+        //     autoplay: true,
+        //     pauseOnHover: false
+        // });
+    }
+
     function toggleOverlay() {
         $('.page-wrapper').toggleClass('js-overlay');
     }
